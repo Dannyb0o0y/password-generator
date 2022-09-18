de943649ea443cb89e861e4f9ee627385616a05a
Dannyb0o0y <danial.rf1999@gmail.com>
2022-09-18 05:06:05 -0400

    added checckboxes for numbers, upperercase and symbols. also linked range and number box

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+document.querySelector('.inputRange').addEventListener('input', updateValue);
+document.getElementById('characterAmountNumber').addEventListener('input', updateValue);
+
+function updateValue (e) {
+  var sibling = e.target.previousElementSibling || e.target.nextElementSibling;
+  sibling.value = e.target.value;
+}
