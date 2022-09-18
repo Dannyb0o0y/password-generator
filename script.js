988b8fa417207e5e22ee20591b659f189ac6dc63
Dannyb0o0y <danial.rf1999@gmail.com>
2022-09-18 05:12:00 -0400

    stopped the form from refreshing the page when clicking generate password

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+document.querySelector(".inputRange").addEventListener("input", updateValue);
+document
+  .getElementById("characterAmountNumber")
+  .addEventListener("input", updateValue);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-document.querySelector('.inputRange').addEventListener('input', updateValue);
-document.getElementById('characterAmountNumber').addEventListener('input', updateValue);
-
-function updateValue (e) {
+function updateValue(e) {
   var sibling = e.target.previousElementSibling || e.target.nextElementSibling;
   sibling.value = e.target.value;
 }
+
+var form = document.getElementById("passwordGeneratorForm");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
